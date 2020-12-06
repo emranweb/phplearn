e0f044c3b5db01cfc307ae72982d5c05d9e930c1
emranweb <emranbd77@gmail.com>
2020-12-06 23:54:26 +0600

    file json

--- file/filewrite.php
+++ file/filewrite.php
@@ -1,7 +1,19 @@
 <?php 
-$filename = "/Users/emran/work/phplearn/file/file2.txt";
+//$filename = "/Users/emran/work/phplearn/file/file2.txt";
+$filename = "D:\\work\\phplearn\\file\\file2.txt";
 
-$fp = fopen($filename, "w");
 
-fwrite($fp, "hello");
+$students = array(
+    array(
+        "fname"=>"emran",
+        "age"=>30,
+    ),
+    array(
+        "fname"=>"sabit",
+        "age"=>30,  
+    )
+);
 
+$encode = json_encode($students);
+
+file_put_contents($filename, $encode, LOCK_EX);
